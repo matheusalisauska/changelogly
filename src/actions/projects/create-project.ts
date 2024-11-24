368eed220e62c9c0b782c3b444774dc113ccddca
Matheus Alisauska <matheusalisauska@gmail.com>
2024-11-24 00:37:11 -0300

    refactor: change error handling

--- src/actions/projects/create-project.ts
+++ src/actions/projects/create-project.ts
@@ -6,24 +6,17 @@ import z from 'zod';
 
 type CreateProject = z.infer<typeof createProjectSchema>;
 
-type CreateProjectResponse =
-  | { success: true }
-  | { success: false; error: string | z.ZodError['errors'] };
-
-export async function createProject(
-  data: CreateProject
-): Promise<CreateProjectResponse> {
+export async function createProject(data: CreateProject) {
   try {
     const result = createProjectSchema.safeParse(data);
 
     if (!result.success) {
       return {
-        success: false,
         error: result.error.errors,
       };
     }
 
-    const response = await fetch(`${env.API_URL}/projects`, {
+    await fetch(`${env.API_URL}/projects`, {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
@@ -31,19 +24,8 @@ export async function createProject(
       },
     });
 
-    if (!response.ok) {
-      const errorMessage = `Erro: ${response.status} - ${response.statusText}`;
-      return {
-        success: false,
-        error: errorMessage,
-      };
-    }
-
-    return { success: true };
+    return 'success';
   } catch (error) {
-    return {
-      success: false,
-      error: error instanceof Error ? error.message : 'Erro desconhecido',
-    };
+    throw new Error(error instanceof Error ? error.message : String(error));
   }
 }
