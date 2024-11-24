7cdff2481003bd67a9a6346e8270a09176b083c8
Matheus Alisauska <matheusalisauska@gmail.com>
2024-11-24 00:35:58 -0300

    feat: add tags in create project schema

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -3,4 +3,5 @@ import z from 'zod';
 export const createProjectSchema = z.object({
   name: z.string().min(1, { message: 'Project Name is required' }),
   description: z.string().min(1, { message: 'Description is required' }),
+  tags: z.array(z.string()).optional(),
 });
