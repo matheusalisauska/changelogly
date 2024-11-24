30d7da5690ab35d8e47c2cb48b41b45a7a0ff1f4
Matheus Alisauska <matheusalisauska@gmail.com>
2024-11-24 00:37:43 -0300

    style: add richColors prop to toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <main className="min-h-screen h-full">{children}</main>
-        <Toaster />
+        <Toaster richColors />
       </body>
     </html>
   );
