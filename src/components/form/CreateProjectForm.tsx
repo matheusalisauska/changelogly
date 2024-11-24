e6e066820afcc26e4ca9145c12bd7b21d2b92356
Matheus Alisauska <matheusalisauska@gmail.com>
2024-11-24 00:37:30 -0300

    feat: finish create project form

--- src/components/form/CreateProjectForm.tsx
+++ src/components/form/CreateProjectForm.tsx
@@ -1,36 +1,62 @@
 'use client';
 
-import { createProjectSchema } from "@/types/schema";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
 import { createProject } from "@/actions/projects/create-project";
+import { createProjectSchema } from "@/types/schema";
+import { ProjectTagsOptions } from "@/utils/constants";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { toast } from "sonner";
+import { z } from "zod";
+import { Button } from "../ui/button";
+import { Combobox } from "../ui/combobox";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import TagBadge from "../TagBadge";
+import { useState } from "react";
+import Link from "next/link";
 
 export const CreateProjectForm = () => {
+    const [selectedTags, setSelectedTags] = useState<{ label: string; value: string }[]>([]);
+
     const form = useForm<z.infer<typeof createProjectSchema>>({
         resolver: zodResolver(createProjectSchema),
         defaultValues: {
             name: "",
             description: "",
+            tags: [],
         },
-    })
+    });
 
     async function onSubmit(data: z.infer<typeof createProjectSchema>) {
-        const response = await createProject(data);
+        const tagValues = selectedTags.map((tag) => tag.value)
 
-        if (!response.success) {
-            return toast.error("Error creating project");
+        const dataWithTags = {
+            ...data,
+            tags: tagValues,
+        };
+
+        try {
+            await createProject(dataWithTags);
+
+            toast.success("Project created successfully");
+        } catch (error) {
+            return toast.error(`Error creating project ${error}`);
         }
+    }
 
-        toast.success("Project created successfully");
+
+    function handleTagSelect(tag: { label: string; value: string }) {
+        if (!selectedTags.find((t) => t.value === tag.value)) {
+            setSelectedTags((prev) => [...prev, tag]);
+        }
+    }
+
+    function handleTagRemove(tagValue: string) {
+        setSelectedTags((prev) => prev.filter((tag) => tag.value !== tagValue));
     }
 
     return (
-        <section className="flex flex-col  gap-y-8">
+        <section className="flex flex-col gap-y-6">
             <h1 className="font-bold text-xl">Create your Project</h1>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
@@ -60,10 +86,40 @@ export const CreateProjectForm = () => {
                             </FormItem>
                         )}
                     />
-                    <Button className="w-full text-base py-5" type="submit">Create</Button>
+                    <FormItem className="flex flex-col">
+                        <FormLabel>Tags</FormLabel>
+                        <FormControl>
+                            <Combobox
+                                options={ProjectTagsOptions}
+                                placeholder="Select the project tags"
+                                onSelect={handleTagSelect}
+                                disabledOptions={selectedTags}
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                    {selectedTags.length > 0 && (
+                        <div className="flex flex-row gap-x-2 items-center">
+                            {selectedTags.map((tag) => (
+                                <TagBadge
+                                    key={tag.value}
+                                    label={tag.label}
+                                    value={tag.value}
+                                    onDelete={() => handleTagRemove(tag.value)}
+                                />
+                            ))}
+                        </div>
+                    )}
+                    <div className="flex flex-col gap-y-4">
+                        <Button className="w-full text-base py-5" type="submit">Create</Button>
+                        <Link href={'/home'}>
+                            <Button className="w-full text-base py-5" variant={'outline'} type="button">
+                                Back to home
+                            </Button>
+                        </Link>
+                    </div>
                 </form>
             </Form>
-
         </section>
-    )
-}
+    );
+};
