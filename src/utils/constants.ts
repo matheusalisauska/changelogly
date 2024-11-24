aaaaa95224c80d22bf3406754869e4573a9bda08
Matheus Alisauska <matheusalisauska@gmail.com>
2024-11-24 00:35:44 -0300

    feat: create constant file

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const ProjectTagsOptions = [
+  { value: 'erp', label: 'ERP' },
+  { value: 'crm', label: 'CRM' },
+  { value: 'proposta', label: 'Proposta' },
+  { value: 'relatorio', label: 'Relatório' },
+];
