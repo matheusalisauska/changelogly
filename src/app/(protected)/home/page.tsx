1edc664db89a078b87dd7bba5c120e483947d6fe
Matheus Alisauska <matheusalisauska@gmail.com>
2024-11-24 00:37:53 -0300

    refactor: remove back link from create project page and streamline project fetching in home page

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -1,13 +1,9 @@
-import { getProjects } from "@/actions/projects/get-projects";
 import { Projects } from "@/components/home/Projects";
+import { getProjects } from "@/utils/data/projects/get-projects";
 
 export default async function Home() {
-  const { error, data: projects } = await getProjects();
+  const projects = await getProjects();
 
-
-  if (error) {
-    return <p>error</p>
-  }
 
   return (
     <main className="flex flex-col flex-1 gap-y-8 h-full w-full">
